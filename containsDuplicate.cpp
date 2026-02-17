86c09ce9afef2a5b8fb5818a9b3425d16a7ebc68
Praveen Kumar <vniharikabsc24@rvu.edu.in>
2026-02-17 20:10:20 +0530

    Added Contains Duplicate program

---
+++ containsDuplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+        
+        for(int i = 0; i < nums.size(); i++) {
+            if(seen.find(nums[i]) != seen.end()) {
+                return true;   // Duplicate found
+            }
+            seen.insert(nums[i]);
+        }
+        
+        return false;  // No duplicates
+    }
